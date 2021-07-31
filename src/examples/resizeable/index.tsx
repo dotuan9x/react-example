0de770c5a16acbb220ffcbbbc758da090872667c
tuan.do1 <tuan.do1@onemount.com>
2021-07-31 10:17:06 +0700

    Add resizeable example

---
+++ src/examples/resizeable/index.tsx
@@ -0,0 +1,152 @@
+// Libraries
+import React, {useState} from 'react';
+import './styles.scss';
+
+interface IDefaultProps {
+
+}
+
+interface ICoordinates {
+    width: number;
+    height: number;
+    top: number;
+    left: number;
+}
+
+const Resizeable = () => {
+    const [coordinates, setCoordinates] = useState<ICoordinates>({
+        width: 200,
+        height: 200,
+        top: 0,
+        left: 0
+    });
+
+    const onMouseDown = (e) => {
+        const resizableTarget = e.target,
+            originalWidth = coordinates.width,
+            originalHeight = coordinates.height,
+            originalX = coordinates.left,
+            originalY = coordinates.top,
+            originalMouseX = e.clientX,
+            originalMouseY = e.clientY;
+
+        const onMouseMove = (e) => {
+            // Top left
+            if (resizableTarget.classList.contains('resizable-nw')) {
+                const resizeWidth = originalWidth - (e.pageX - originalMouseX);
+                const resizeHeight = originalHeight - (e.pageY - originalMouseY);
+
+                setCoordinates({
+                    width: Math.round(resizeWidth),
+                    height: Math.round(resizeHeight),
+                    left: coordinates.left - Math.round(resizeWidth) + coordinates.width,
+                    top: coordinates.top - Math.round(resizeHeight) + coordinates.height
+                });
+            }
+
+            // Top
+            if (resizableTarget.classList.contains('resizable-n')) {
+                const height = originalHeight - (e.pageY - originalMouseY);
+
+                setCoordinates({
+                    ...coordinates,
+                    height: Math.round(height),
+                    top: coordinates.top - Math.round(height) + coordinates.height
+                });
+            }
+
+            // Top Right
+            if (resizableTarget.classList.contains('resizable-ne')) {
+                const width = originalWidth + (e.pageX - originalMouseX);
+                const height = originalHeight - (e.pageY - originalMouseY);
+
+                setCoordinates({
+                    ...coordinates,
+                    width: Math.round(width),
+                    height: Math.round(height),
+                    top: coordinates.top - Math.round(height) + coordinates.height
+                });
+            }
+
+            // Right
+            if (resizableTarget.classList.contains('resizable-e')) {
+                const width = originalWidth + (e.pageX - originalMouseX);
+
+                setCoordinates({
+                    ...coordinates,
+                    width: Math.round(width)
+                });
+            }
+
+            // Bottom Right
+            if (resizableTarget.classList.contains('resizable-se')) {
+                const width = originalWidth + (e.pageX - originalMouseX);
+                const height = originalHeight + (e.pageY - originalMouseY);
+
+                setCoordinates({
+                    ...coordinates,
+                    width: Math.round(Math.abs(width)),
+                    height: Math.round(Math.abs(height))
+                });
+            }
+
+            // Bottom
+            if (resizableTarget.classList.contains('resizable-s')) {
+                const height = originalHeight + (e.pageY - originalMouseY);
+
+                setCoordinates({
+                    ...coordinates,
+                    height: Math.round(height)
+                });
+            }
+
+            // Bottom Left
+            if (resizableTarget.classList.contains('resizable-sw')) {
+                const height = originalHeight + (e.pageY - originalMouseY);
+                const width = originalWidth - (e.pageX - originalMouseX);
+
+                setCoordinates({
+                    ...coordinates,
+                    width: Math.round(width),
+                    height: Math.round(height),
+                    left: coordinates.left - Math.round(width) + coordinates.width
+                });
+            }
+
+            // Left
+            if (resizableTarget.classList.contains('resizable-w')) {
+                const width = originalWidth - (e.pageX - originalMouseX);
+
+                setCoordinates({
+                    ...coordinates,
+                    width: Math.round(width),
+                    left: coordinates.left - Math.round(width) + coordinates.width
+                });
+            }
+        };
+
+        const onMouseUp = () => {
+            window.removeEventListener('mousemove', onMouseMove);
+            window.removeEventListener('mouseup', onMouseUp);
+        };
+
+        window.addEventListener('mousemove', onMouseMove);
+
+        window.addEventListener('mouseup', onMouseUp);
+    };
+
+    return (
+        <div style={{top: coordinates.top, left: coordinates.left, width: coordinates.width, height: coordinates.height}} className="resizable m-10">
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-nw" />
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-ne" />
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-sw" />
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-se" />
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-n" />
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-s" />
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-e" />
+            <div onMouseDown={onMouseDown}  className="resizable-handle resizable-w" />
+        </div>
+    );
+};
+
+export default Resizeable;
--- src/modules/Layouts/index.tsx
+++ src/modules/Layouts/index.tsx
@@ -3,94 +3,12 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 
 // Components
-// import DefaultMain from 'Modules/Layouts/containers/DefaultMain';
-import {Button} from 'Components/Button';
+import Resizeable from 'Src/examples/resizeable';
 
 const Layouts = () => {
     return (
         <div className="flex flex-col">
-            <Button width={200} />
-            {/* <div className="px-2 py-2 bg-blue-500">
-                <div className="flex flex-row">
-                    <div className="flex-1">
-                        <a href="#">
-                            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
-                        </a>
-                    </div>
-                    <div className="flex-1 flex flex-row items-center text-center relative bg-white opacity-80 hover:opacity-100 rounded-sm">
-                        <div className="text-blue-500 px-2 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <input placeholder="Search" className="flex-1 block h-full px-2 outline-none" />
-                        <div className="text-blue-500 px-2 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                            </svg>
-                        </div>
-                    </div>
-                    <div className="flex-1 content-center items-center">
-                        <div className="flex flex-row-reverse">
-                            <button className="text-white order-2">
-                                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                                </svg>
-                            </button>
-                            <button className="order-1 ml-3 focus:outline-none focus:ring-2">
-                                <img className="h-8 w-8 rounded-full"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="" />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div className="flex flex-row">
-                <div className="fixed top-12 bottom-0 bg-gray-100 w-80 text-sm subpixel-antialiased	">
-                    <div className="flex items-center h-16 text-blue-500 cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-                        </svg>
-                    </div>
-                    <ul>
-                        <li className="flex flex-row items-center p-2 bg-gray-200 hover:bg-gray-200 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
-                            <span className="ml-2">Ngày của tôi</span>
-                        </li>
-                        <li className="flex flex-row items-center p-2 hover:bg-gray-200 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-                            </svg>
-                            <span className="ml-2">Quan trọng</span>
-                        </li>
-                        <li className="flex flex-row items-center p-2 hover:bg-gray-200 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                            <span className="ml-2">Đã lên kế hoạch</span>
-                        </li>
-                        <li className="flex flex-row items-center p-2 hover:bg-gray-200 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                            <span className="ml-2">Tác vụ</span>
-                        </li>
-                        <li className="flex flex-row items-center p-2 hover:bg-gray-200 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                            <span className="ml-2">Hôm nay</span>
-                        </li>
-                    </ul>
-                </div>
-                <div className="" />
-            </div> */}
+            <Resizeable />
         </div>
     );
 };
